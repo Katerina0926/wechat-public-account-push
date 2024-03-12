6007acb9855921a11bfafff7c6f6d3836223fc90
KomaOvO <104199092+Katerina0926@users.noreply.github.com>
2024-03-12 17:59:31 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -48,16 +48,6 @@ const USER_CONFIG = {
   ],
 
 
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'beQPjm6iQQ5v7mvZ47d9OC69BfyTU0vJiyQpC-DkCz8',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o0YL56H_Bj4C06q7yFfH1nbkfTc0',
-    }
-  ],
 
 }
 
