19d761e81d3e16c7acc668ea81f69c9a430f698f
KomaOvO <104199092+Katerina0926@users.noreply.github.com>
2024-03-25 15:57:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o0YL56H_Bj4C06q7yFfH1nbkfTc0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'st5jBsPQeyFBN4xFy7ddbHPtGk1easuJ5iuhPypR3II',
+      useTemplateId: 'I0mVQYtxDzGxw73VBSaprCQkhT3NrbnXLyShXOJDgcQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-01',
       festivals: [
@@ -35,14 +35,20 @@ const USER_CONFIG = {
         {
           type: '生日', name: '李四', year: '1996', date: '09-31',
         },
+        {
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+        },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-12-03' },
+        // 结婚纪念日
       ],
     },
   ],
+
+
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '',
 
@@ -57,5 +63,3 @@ const USER_CONFIG = {
 }
 
 module.exports = USER_CONFIG
-
-
