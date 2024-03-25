62eb769bbf1b4caed25441038fdd2703f856a891
KomaOvO <104199092+Katerina0926@users.noreply.github.com>
2024-03-25 16:49:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o0YL56H_Bj4C06q7yFfH1nbkfTc0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'KDIESPnHHPitT1KoIL0OHWlJ37hRFzeZq1tM6lgOU-o',
+      useTemplateId: 'Uc2s4sDTTtDJc1G07zmybdRojog4t7o0LXouHXQ9_Wo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-01',
       festivals: [
