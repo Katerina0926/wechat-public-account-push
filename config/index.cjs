878fcda42255fa87fa3828827917393aefe518e7
KomaOvO <104199092+Katerina0926@users.noreply.github.com>
2024-05-24 15:42:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,27 +13,27 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '755138caea208e6d0a21a2c182e70b18',
 
-  PROVINCE: '广东',
-  CITY: '阳江',
+  PROVINCE: '',
+  CITY: '',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '小茗',
+      name: '',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o0YL56P3lr6naoNSy544j-Txlxgc',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'p9RbaTwAzl0Y1Go1GId6K_dwjXrWo3IVixM5umdEVGs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '01-01',
+      horoscopeDate: '',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '小茗宝宝', year: '2000', date: '01-01',
+          type: '*生日', name: '', year: '2000', date: '01-01',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '小茗宝宝', year: '2000', date: '01-01',
+          type: '生日', name: '', year: '2000', date: '01-01',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
@@ -42,9 +42,9 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-12-03' },
+        { keyword: 'love_day', date: '' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '' },
       ],
     },
   ],
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o0YL56H_Bj4C06q7yFfH1nbkfTc0',
+      id: '',
     }
   ],
 
